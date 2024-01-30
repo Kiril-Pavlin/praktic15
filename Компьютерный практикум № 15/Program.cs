cda6990682bbcbf39290be46742814d52d2da6b6
Kiril-Pavlin <153673241+Kiril-Pavlin@users.noreply.github.com>
2024-01-30 22:36:43 +0200

    Add files via upload

---
+++ Компьютерный практикум № 15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компьютерный_практикум___15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"x\ty");
+            Console.WriteLine();
+            for (double x = 0; x <= 5.2; x += 0.2)
+            {
+                double y = Math.Sin(x);
+                Console.Write(x);
+                Console.WriteLine($"\t{y:f2}");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
